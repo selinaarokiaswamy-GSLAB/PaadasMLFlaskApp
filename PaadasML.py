5783ba8e5eb76f7fcb82fbedb8c7084ddbe65427
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-05-18 16:10:58 +0530

    Update PaadasML.py

--- PaadasML.py
+++ PaadasML.py
@@ -1,9 +1,11 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, Response
+from flask import Flask, send_file, Response, render_template, redirect, url_for, request
 import random
 import time
 import wave
+import io
+import os
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -12,9 +14,10 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
+# ‘/’ URL is bound with index() function.
 @app.route('/')
-# ‘/’ URL is bound with paadas() function.
 def paadas():
+    #Approach 2
     def generate(files):
         with wave.open(files[0], 'rb') as f:
             params = f.getparams()
@@ -37,8 +40,28 @@ def paadas():
     files.append("../numbers/" + str(number) + ".wav")
     times = random.randint(1,10)
     files.append("../times/" + str(times) + ".wav")
-    return render_template("index.html", source=generate(files))
-   
+    data = dict(
+        file=(generate(files), "padaa.wav"),
+    )
+
+    #app.post(url_for('static', filename='padaa.wav'), content_type='multipart/form-data', data=data)
+    print ('posted data on client\n')
+    return render_template("index.html")
+
+@app.route("/recording", methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        f = open('./file.wav', 'wb')
+        print(request)
+        f.write(request.data)
+        f.close()
+        if os.path.isfile('./file.wav'):
+            print("./file.wav exists")
+
+        return render_template('index.html', request="POST")   
+    else:
+        return render_template("index.html")
+ 
 # main driver function
 if __name__ == '__main__':
  
