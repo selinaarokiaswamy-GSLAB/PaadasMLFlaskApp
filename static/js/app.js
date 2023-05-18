98d25439339153c87ca3a39ad3825fde94688a59
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-05-18 16:09:06 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -111,57 +111,13 @@ function stopRecording() {
     //stop microphone access
     gumStream.getAudioTracks()[0].stop();
 
-    //create the wav blob and pass it on to createDownloadLink
-    rec.exportWAV(createDownloadLink);
+    //create the wav blob and pass it to the server
+    rec.exportWAV(sendAudioData)
 }
 
-function createDownloadLink(blob) {
-
-    var url = URL.createObjectURL(blob);
-    var au = document.createElement('audio');
-    var li = document.createElement('li');
-    var link = document.createElement('a');
-
-    //name of .wav file to use during upload and download (without extendion)
-    var filename = new Date().toISOString();
-
-    //add controls to the <audio> element
-    au.controls = true;
-    au.src = url;
-
-    //save to disk link
-    link.href = url;
-    link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-    link.innerHTML = "Save to disk";
-
-    //add the new audio element to li
-    li.appendChild(au);
-
-    //add the filename to the li
-    li.appendChild(document.createTextNode(filename+".wav "))
-
-    //add the save to disk link to li
-    li.appendChild(link);
-
-    //upload link
-    var upload = document.createElement('a');
-    upload.href="#";
-    upload.innerHTML = "Upload";
-    upload.addEventListener("click", function(event){
-          var xhr=new XMLHttpRequest();
-          xhr.onload=function(e) {
-              if(this.readyState === 4) {
-                  console.log("Server returned: ",e.target.responseText);
-              }
-          };
-          var fd=new FormData();
-          fd.append("audio_data",blob, filename);
-          xhr.open("POST","/",true);
-          xhr.send(fd);
-    })
-    li.appendChild(document.createTextNode (" "))//add a space in between
-    li.appendChild(upload)//add the upload link to li
-
-    //add the li element to the ol
-    recordingsList.appendChild(li);
+function sendAudioData(audioBlob) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://127.0.0.1:5000/recording");
+  xhr.setRequestHeader("Content-Type", "audio/wav")
+  xhr.send(audioBlob)
 }
