92dd4e6bbe70f6141ed8e9cf318bbd9ebe258591
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-05-20 18:23:05 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -11,12 +11,10 @@ var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
     console.log("recordButton clicked");
@@ -34,7 +32,6 @@ function startRecording() {
 
     recordButton.disabled = true;
     stopButton.disabled = false;
-    pauseButton.disabled = false
 
     /*
         We're using the standard promise based getUserMedia() 
@@ -76,22 +73,7 @@ function startRecording() {
         //enable the record button if getUserMedia() fails
         recordButton.disabled = false;
         stopButton.disabled = true;
-        pauseButton.disabled = true
     });
-}
-
-function pauseRecording(){
-    console.log("pauseButton clicked rec.recording=",rec.recording );
-    if (rec.recording){
-        //pause
-        rec.stop();
-        pauseButton.innerHTML="Resume";
-    }else{
-        //resume
-        rec.record()
-        pauseButton.innerHTML="Pause";
-
-    }
 }
 
 function stopRecording() {
@@ -100,10 +82,7 @@ function stopRecording() {
     //disable the stop button, enable the record too allow for new recordings
     stopButton.disabled = true;
     recordButton.disabled = false;
-    pauseButton.disabled = true;
-
-    //reset button just in case the recording is stopped while paused
-    pauseButton.innerHTML="Pause";
+   
 
     //tell the recorder to stop the recording
     rec.stop();
@@ -116,12 +95,13 @@ function stopRecording() {
 }
 
 function sendAudioData(audioBlob) {
+  console.log("sending audio data");
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "http://127.0.0.1:5000/");
   xhr.setRequestHeader("Content-Type", "audio/wav")
   xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
-      document.write(xhr.responseText)
+      document.write(xhr.responseText);
     }
   }
   xhr.send(audioBlob)
